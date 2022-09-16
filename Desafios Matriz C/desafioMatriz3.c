29a97c1d031d59a002630adf407388f7f1731987
Matheus Della <73204305+MDellaCS@users.noreply.github.com>
2022-09-16 19:27:02 -0300

    Add files via upload

---
+++ Desafios Matriz C/desafioMatriz3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (){
+    
+    int m[6][6]; 
+    
+    for(int i=0;i<6;i++){
+        
+        for(int j=0;j<6;j++){
+            
+            if(i==j || i>j){
+                
+                m[i][j] = 1;
+ 
+            }else{
+                
+                m[i][j] = 0;
+            
+            }
+            
+            if((i+j)==0){
+                
+                m[i][j] = 5;
+                
+            }
+            
+            printf("%d " , m[i][j]);
+            
+        }
+        
+        printf("\n");
+        
+    }
+    
+  return 0;
+}
---
+++ Desafios Matriz C/desafioMatriz7.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (){
+    
+    int m[10][10]; 
+    
+    for(int i=0;i<10;i++){
+        
+        for(int j=0;j<10;j++){
+            
+            if(i==j || i<j){
+                
+                m[i][j] = 1;
+ 
+            }else{
+                
+                m[i][j] = 0;
+            
+            }
+            
+            printf("%d " , m[i][j]);
+            
+        }
+        
+        printf("\n");
+        
+    }
+    
+  return 0;
+}
